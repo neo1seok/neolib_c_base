0b21b189799974f6092a7721d303ae697b89176e
neo1seok <wsshin@ictk.com>
2018-02-08 05:20:50 +0000

    update make file

--- sample/build.sh
+++ sample/build.sh
@@ -1,4 +1,3 @@
-g++ -o testd test.cpp -L../lib/gnu -lIUST-100d -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
-g++ -o test test.cpp -L../lib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG_
+g++ -o test test.cpp -L../lib/gnu -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -I../include
 
 #g++ -o test test.c -Llib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
