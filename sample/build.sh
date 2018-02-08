22c73ee77f7f8d4f1f10454a419b8d3aea2e136c
neo1seok <neo1seok@gmail.com>
2018-02-08 13:57:05 +0900

    add sample source

---
+++ sample/build.sh
@@ -0,0 +1,4 @@
+g++ -o testd test.cpp -L../lib/gnu -lIUST-100d -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
+g++ -o test test.cpp -L../lib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG_
+
+#g++ -o test test.c -Llib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
