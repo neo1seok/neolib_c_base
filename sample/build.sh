eede67641c8108806ff7f04d7072ed8383ca2479
neo1seok <wsshin@ictk.com>
2018-03-28 11:17:11 +0900

    change makefile

--- sample/build.sh
+++ sample/build.sh
@@ -1,3 +1,4 @@
-g++ -o test test.cpp -L../lib/gnu -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -I../include
+g++ -o test test.cpp -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
 
+#g++ -o test test.cpp -L../lib/gnu -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
 #g++ -o test test.c -Llib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
