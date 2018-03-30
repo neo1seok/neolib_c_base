341adb37aab9f6d9cad5e5c70c332a7f099ccc4a
neo1seok <wsshin@ictk.com>
2018-03-30 17:57:51 +0900

    update from pi

--- sample/build.sh
+++ sample/build.sh
@@ -1,4 +1,5 @@
-g++ -o test test.cpp -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
+g++ -o test test.cpp -lneo_c_lib -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
+g++ -o test2 test.cpp /usr/local/lib/libneo_c_lib.a -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
 
 #g++ -o test test.cpp -L../lib/gnu -lneolib_c_base -ldl -D_DEFNEODEF_  -DNEODEBUG_ -Ineo_c_lib
 #g++ -o test test.c -Llib/gnu -lIUST-100 -lslab_usb_spi -ldl -D_DEFNEODEF_  -DNEODEBUG
