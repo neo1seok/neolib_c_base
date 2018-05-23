77c17cbdc80d5fde76bccee2a1019f6e4a14bb60
neo1seok <neo1seok@gmail.com>
2018-05-23 16:19:35 +0900

    add python publish script adopt build for using python

---
+++ neo_c_lib/publish_include.py
@@ -0,0 +1,25 @@
+#! /usr/local/bin/python3
+import sys
+import shutil
+print(sys.path)
+print(sys.argv)
+is_no_force = sys.argv.__len__() >1 and sys.argv[1] == "-f"
+print("is_no_force",is_no_force)	
+str_list_incldue ="""comdef.h
+CSerialBASE.h
+CSerialRS232.h
+neoCoLib.h
+neoDebug.h
+NeoSystemLib.h"""
+list_include = str_list_incldue.split("\n")
+print(list_include)
+if not is_no_force:
+	input("ready to copy?")
+for inc_file in list_include:
+	org_file = "include/"+inc_file
+	dst_file = "../include/"+inc_file
+	print("copy",org_file,"to",dst_file)
+	shutil.copy(org_file,dst_file)
+if not is_no_force:
+	input("ok")	
+
