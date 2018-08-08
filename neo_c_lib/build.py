0db222f62f33a7dcbe1ce1eedc10e7708193091f
neo1seok <neo1seok@gmail.com>
2018-08-08 16:22:17 +0900

    update build.py

--- neo_c_lib/build.py
+++ neo_c_lib/build.py
@@ -2,11 +2,11 @@
 import os
 import  platform
 
-os = platform.system()
-if os == "Linux:
-	list_makefile = ["Makefile.x64","Makefile.arm","Makefile.x86"]
+#os = platform.system()
+#if os == "Linux:
+list_makefile = ["Makefile.x64","Makefile.armhf","Makefile.x86"]
 
-	for tmp in list_makefile:
-		os.system("make -f %s clean"%tmp)
-		os.system("make -f %s"%tmp)
+for tmp in list_makefile:
+	os.system("make -f %s clean"%tmp)
+	os.system("make -f %s"%tmp)
 		
